6bea56bc0c92cf2365e6ccaffa7e161be6ac47c9
gian236 <gianparobelo@ufm.edu>
2025-10-04 01:13:18 -0600

    component experiencia

--- cv-angular/src/main.ts
+++ cv-angular/src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { provideAnimations } from '@angular/platform-browser/animations'; // <--- importar
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  ...appConfig,
+  providers: [
+    ...(appConfig.providers || []), // conservar los providers existentes
+    provideAnimations()             // <--- habilitar animaciones
+  ]
+}).catch((err) => console.error(err));
