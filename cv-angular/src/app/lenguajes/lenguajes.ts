7f93d4da23f36f4418a9776b875b5620bcecc500
gian236 <gianparobelo@ufm.edu>
2025-10-03 23:07:39 -0600

    component lenguajes

--- cv-angular/src/app/lenguajes/lenguajes.ts
+++ cv-angular/src/app/lenguajes/lenguajes.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-lenguajes',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './lenguajes.html',
   styleUrl: './lenguajes.css'
 })
-export class Lenguajes {
-
-}
+export class Lenguajes {}
