8ab3fe075e7d239fdefbdd939b764367e95fb17d
gian236 <gianparobelo@ufm.edu>
2025-10-03 23:42:24 -0600

    component tecnologías

--- cv-angular/src/app/tecnologias/tecnologias.ts
+++ cv-angular/src/app/tecnologias/tecnologias.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tecnologias',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './tecnologias.html',
-  styleUrl: './tecnologias.css'
+  styleUrls: ['./tecnologias.css']
 })
 export class Tecnologias {
-
+  basesDeDatos: string[] = ['Redis', 'ElasticSearch', 'PostgreSQL', 'SQLite', 'MySQL', 'MongoDB'];
+  contenedores: string[] = ['Docker'];
+  diseno: string[] = ['Figma'];
+  nube: string[] = ['AWS', 'S3', 'Azure'];
 }
