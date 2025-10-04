52e585f68763017fbf6202449b750cf245b37981
gian236 <gianparobelo@ufm.edu>
2025-10-03 22:22:53 -0600

    creado el angular project y component header

---
+++ cv-angular/src/app/lenguajes/lenguajes.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lenguajes',
+  imports: [],
+  templateUrl: './lenguajes.html',
+  styleUrl: './lenguajes.css'
+})
+export class Lenguajes {
+
+}
